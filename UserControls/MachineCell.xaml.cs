811cc50a9c07e9ed65d4d99fb8228ff2a74ca089
Денис <@DESKTOP-I1S9F8L>
2023-05-04 16:51:43 +0500

    Добавьте файлы проекта.

---
+++ UserControls/MachineCell.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TuringMachineEmulator.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для MachineCell.xaml
+    /// </summary>
+    public partial class MachineCell : UserControl
+    {
+        public MachineCell()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        public string CellValue { get; set; }
+        
+        public string Number { get; set; }
+
+        private void cellValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (cellValue.Text == "")
+            {
+                cellValue.Text = "#";
+            }
+        }
+    }
+}
